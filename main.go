45bbf15091f64b90560a88e33eb983f428d35124
Ekko <beijiu572@gmail.com>
2025-06-15 11:29:36 +0800

    feat: compatible with NetEase bmh https://bmh.163.com/

--- main.go
+++ main.go
@@ -504,7 +504,8 @@ func hookDetails(resp *http.Response) error {
 	parentId := resp.Request.URL.Query().Get("ParentId")
 	lib, ok := libraryMap[parentId]
 	if !ok {
-		return nil
+		// 网易爆米花通过 Users/xxx/Items 获取数据，所以需要特殊处理
+		return hookViews(resp)
 	}
 	bodyText := getItems(lib, resp.Request, nil)
 	bodyBytes, err := json.Marshal(bodyText)
